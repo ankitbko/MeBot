e5c7f1794126c028ff769c98f72834ddafa98ce8
Ankit <ankitbko@gmail.com>
2016-08-23 14:24:50 +0530

    Added None and AboutMe intent handlers

--- MeBot/Controllers/MessagesController.cs
+++ MeBot/Controllers/MessagesController.cs
@@ -7,6 +7,8 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using MeBot.Dialogs;
 
 namespace MeBot
 {
@@ -21,13 +23,7 @@ namespace MeBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                await Conversation.SendAsync(activity, () => new MeBotLuisDialog());
             }
             else
             {
---
+++ MeBot/Dialogs/MeBotLuisDialog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Luis;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace MeBot.Dialogs
+{
+    [LuisModel("modelid", "subkey")]
+    [Serializable]
+    public class MeBotLuisDialog : LuisDialog<object>
+    {
+        public MeBotLuisDialog(params ILuisService[] services) : base(services)
+        {
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I'm sorry. I didn't understand you.");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("AboutMe")]
+        public async Task AboutMe(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync(@"Ankit is a Software Engineer currently working in Microsoft Center of Excellence team at Mindtree. 
+                He started his professional career in 2013 after completing his graduation as Bachelor in Computer Science.");
+            await context.PostAsync(@"He is a technology enthusiast and loves to dig in emerging technologies. 
+                Most of his working hours are spent on creating architecture, evaluating upcoming products and developing frameworks.");
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
